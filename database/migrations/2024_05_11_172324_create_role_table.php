771c299d42931b8c953b7c9d8a23ba3cca728654
ikmal <fajrikmal141@gmail.com>
2024-05-12 00:42:31 +0700

    level3

--- database/migrations/2024_05_11_172324_create_role_table.php
+++ database/migrations/2024_05_11_172324_create_role_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('level');
             $table->string('level2');
+            $table->string('level3');
             $table->timestamps();
         });
     }
