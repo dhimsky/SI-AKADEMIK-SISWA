77f1fbf4cf07196046344c23cdfe72ec1a69a2b3
ikmal <fajrikmal141@gmail.com>
2024-05-13 14:20:56 +0700

    route lagi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
             $request->session()->regenerate();
             $user = Auth::user();
             if ($user->role_id == '1') {
-                return redirect()->intended()->route('stafftu.dashboard')->with('toast_success','Selamat, anda berhasil masuk.');
+                return redirect()->route('stafftu.dashboard')->with('toast_success','Selamat, anda berhasil masuk.');
             } elseif ($user->role_id == '2') {
                 return redirect()->intended('home')->with('toast_success','Selamat, anda berhasil masuk.');
             }
