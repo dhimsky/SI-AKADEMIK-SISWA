32edd7b36b2669d3cff068ff84bc16c52a8c06ac
Jahats404 <fajrikmal141@gmail.com>
2024-07-01 11:17:50 +0700

    update absensi

--- app/Http/Controllers/Export/AbsensiPdfController.php
+++ app/Http/Controllers/Export/AbsensiPdfController.php
@@ -18,31 +18,35 @@ class AbsensiPdfController extends Controller
             ->whereYear('tanggal_absensi', $bulanTahun->year)
             ->whereMonth('tanggal_absensi', $bulanTahun->month)
             ->get();
-        dd($absensi);
-        $atribut = Absensi::where('kelas', $kelas_id)->first();
-        $semester = $atribut->semester;
 
-        // Mengatur lokal ke bahasa Indonesia
-        Carbon::setLocale('id');
-
-        $date = $atribut->tanggal_absensi;
-        $carbonDate = Carbon::parse($date);
-
-        // Format tanggal dalam bahasa Indonesia
-        setlocale(LC_TIME, 'id_ID');
-        $bulan = $carbonDate->translatedFormat('F'); // Output: Juni
-
-        // Ambil semua tanggal absensi untuk bulan yang bersangkutan
-        $daysInMonth = $carbonDate->daysInMonth;
-
-        $absensiData = [];
-        foreach ($absensi as $a) {
-            $day = date('d', strtotime($a->tanggal_absensi));
-            $absensiData[$a->nis_id]['nama'] = $a->siswa->nama_siswa;
-            $absensiData[$a->nis_id]['status'][$day] = $a->status_absensi;
+        if ($absensi) {
+            $atribut = Absensi::where('kelas', $kelas_id)->first();
+            $semester = $atribut->semester;
+    
+            // Mengatur lokal ke bahasa Indonesia
+            Carbon::setLocale('id');
+    
+            $date = $request->tanggal_absensi;
+            $carbonDate = Carbon::parse($date);
+    
+            // Format tanggal dalam bahasa Indonesia
+            setlocale(LC_TIME, 'id_ID');
+            $bulan = $carbonDate->translatedFormat('F'); // Output: Juni
+            // dd($bulan);
+            // Ambil semua tanggal absensi untuk bulan yang bersangkutan
+            $daysInMonth = $carbonDate->daysInMonth;
+    
+            $absensiData = [];
+            foreach ($absensi as $a) {
+                $day = date('d', strtotime($a->tanggal_absensi));
+                $absensiData[$a->nis_id]['nama'] = $a->siswa->nama_siswa;
+                $absensiData[$a->nis_id]['status'][$day] = $a->status_absensi;
+            }
+            
+            $pdf = PDF::loadView('admin.absensi.absensi-pdf', compact('absensiData', 'kelas_id', 'semester', 'bulan', 'daysInMonth'))->setPaper('a4', 'landscape');
+            return $pdf->stream('absensi_siswa.pdf');
+        }else {
+            return redirect()->back()->with('error', 'Tidak ada data');
         }
-        
-        $pdf = PDF::loadView('admin.absensi.absensi-pdf', compact('absensiData', 'kelas_id', 'semester', 'bulan', 'daysInMonth'))->setPaper('a4', 'landscape');
-        return $pdf->stream('absensi_siswa.pdf');
     }
 }
